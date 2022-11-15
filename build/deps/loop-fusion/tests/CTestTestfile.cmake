e61f204c848a0f4318137bbff7aa5251c2d4517e
Floyd Zweydinger <floyd.zweydinger@rub.de>
2022-11-15 08:59:43 +0100

    removed build files

--- build/deps/loop-fusion/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/duda/Downloads/programming/loop-fusion-test/deps/loop-fusion/tests
-# Build directory: /home/duda/Downloads/programming/loop-fusion-test/build/deps/loop-fusion/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/duda/Downloads/programming/loop-fusion-test/build/deps/loop-fusion/tests/loop_fusion_test_include-b858cb2.cmake")
